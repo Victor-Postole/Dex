f5eb0c8d4ef59ca92c4d88f5a3e7aa31e0e1f8ac
disesosoftware <pstlvictoras@yahoo.com>
2025-04-24 11:54:14 +0300

    Initial Commit

---
+++ Dex/DexApp.swift
@@ -0,0 +1,20 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by diseso software on 24.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
