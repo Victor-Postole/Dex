4fb48e55eca5b2e0172fd43f470f1cc504a84de9
disesosoftware <pstlvictoras@yahoo.com>
2025-04-28 14:12:03 +0300

    add widget extension

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by diseso software on 28.04.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
